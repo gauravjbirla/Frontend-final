e6ffa12becf80517fd0eee3759be201474916940
Gaurav Birla <72148864+gauravjbirla@users.noreply.github.com>
2024-07-23 16:35:15 +0530

    Update apiCall.js

--- src/apis/apiCall.js
+++ src/apis/apiCall.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
     //baseURL:'http://localhost:4000'
-    baseURL:'https://backend-final-b13v.onrender.com/loaddata'
+    baseURL:'https://backend-final-b13v.onrender.com/'
 }) 
